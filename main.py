9425db76510aed5ad4cf97817481bf83645f2ac1
vjimcor955 <vjimcor955@g.educaand.es>
2024-11-28 17:58:38 +0100

    add middleware CORS and allow client origin

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import petitionRoutes
+from routers import userRoutes
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://fastapi-client.netlify.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Peticiones FastAPI": "Grupo 3"}
 
 app.include_router(petitionRoutes.router)
+app.include_router(userRoutes.router)
